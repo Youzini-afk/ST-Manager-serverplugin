4dfeace4bb602bbc987969be16e59d0cd6dbae76
Youzini-afk <13153778771cx@gmail.com>
2026-02-03 21:05:40 +0800

    Cleanup: Remove old project files but keep history

--- core/consts.py
+++
@@ -1,5 +0,0 @@
-# 伴生图扩展名
-SIDECAR_EXTENSIONS = ['.png', '.webp', '.jpg', '.jpeg']
-
-# 保留的资源目录名称 (禁止用户创建或关联)
-RESERVED_RESOURCE_NAMES = {'notes', 'backups', 'lorebooks', 'thumbnails', 'cards', 'trash'}
